817dd3c8a36444a5d0f856eb7d06b60b2ba9d94c
Quang Ly <quang_ly@comcast.com>
2017-03-25 20:35:17 -0400

    initial commit

---
+++ cat_server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var catRoutes = require('./routes/cat.js')(app);
+var myport = 3000
+var server = app.listen(myport, function () {
+    console.log('Server running at http://127.0.0.1:'+myport+'/');
+});
